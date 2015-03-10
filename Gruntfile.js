57a317e73b304e7a0e1e714e149dfb293028e236
Jasper Nalbach <jasper@bluetreehouse.de>
2015-03-10 14:56:52 +0100

    new gruntfile: fixed file order and minified blueleaf.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,29 +1,37 @@
 module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        concat:{
+        concat: {
             'public_html/development/libs/blueleaf/js/blueleaf.js': [
+                'public_html/development/libs/blueleaf/js-source/pluginapi.js',
+                'public_html/development/libs/blueleaf/js-source/customrules.js',
                 'public_html/development/libs/blueleaf/js-source/*.js',
-                '!public_html/development/libs/blueleaf/js-source/blueleaf.js',
-                'public_html/development/libs/blueleaf/js-source/blueleaf.js',
+                '!public_html/development/libs/blueleaf/js-source/core.js',
+                'public_html/development/libs/blueleaf/js-source/core.js',
             ]
         },
-        comments: {
-            build: {
-              options: {
-                  singleline: false,
-                  multiline: false
-              },
-              src: [ 'public_html/development/libs/blueleaf/js/blueleaf.js']
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+            },
+            dist: {
+                files: {
+                    'public_html/development/libs/blueleaf/js/blueleaf.js': ['public_html/development/libs/blueleaf/js/blueleaf.js']
+                }
             }
         }
     });
     grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-stripcomments');
-    
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
     grunt.registerTask(
-        'build',
-        'Compiles all the assets and copies the files to the build directory.',
-       [ 'concat', 'comments' ]
-    );
+            'build',
+            'Compiles all the assets and copies the files to the build directory.',
+            ['concat']
+            );
+    grunt.registerTask(
+            'cleanbuild',
+            'Compiles all the assets and copies the files to the build directory.',
+            ['concat', 'uglify']
+            );
 };
